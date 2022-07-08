5000bde9b7bd52947a7bae5c479e328a68bf976c
chandu806 <76252021+chandu806@users.noreply.github.com>
2022-07-08 21:14:06 +0530

    j

--- src/Components/store.jsx
+++ src/Components/store.jsx
@@ -3,8 +3,8 @@ const Store = [
     {id: 2, name: 'Task with dropdown menu', label:'NeW', author:'By Jonny'},
     {id:3, name: 'Badge on the right task', label:'LATEST TASK', author:'This task has show on hover action'},
     {id:4, name: 'Go grocery shoppingg',label:'rejected', author:'A short description for this todo item'},
-    {id: 4, name: 'Wash the Car', label: 'Rejected', author: 'Written by Bob'},
-    {id: 5, name: 'Service the Car', label: 'Rejected', author: 'Written by Bob'},
+    {id: 5, name: 'Wash the Car', label: 'Rejected', author: 'Written by Bob'},
+    {id: 6, name: 'Service the Car', label: 'Rejected', author: 'Written by Bob'},
 ]
 
 export {Store}
